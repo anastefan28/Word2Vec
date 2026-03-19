b1604fa38a13bd1cf8e709c5a6fdd28b0233f24a
Ana Stefan <anastefana28@gmail.com>
2026-03-19 18:09:34 +0200

    Implement model functions for training

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from collections import Counter
 
 import numpy as np
+from model import init_vectors
 from data import tokenise, build_vocab, build_noise_distribution, generate_skipgram_pairs
 
 with open("shakespeare.txt") as f:
@@ -27,7 +28,6 @@ for i, word in enumerate(id2word[:10]):
 embed_dim = 50
 vocab_size = len(word2id)
 
-
 corpus_ids = [word2id[w] for w in tokens if w in word2id]
 pairs = generate_skipgram_pairs(corpus_ids, window=2)
 
@@ -35,4 +35,11 @@ print(f"Total training pairs: {len(pairs)}")
 print("First 10 pairs:")
 for word, neighbour in pairs[:10]:
     print(f"({id2word[word]}, {id2word[neighbour]})")
+
+input_vectors, output_vectors = init_vectors(vocab_size, embed_dim)
+  
+# print(f"{input_vectors[0][:5]}")   
+# print(f"{output_vectors[0][:5]}")   
+
+
 
---
+++ model.py
@@ -0,0 +1,52 @@
+import numpy as np
+
+def init_vectors(vocab_size, dim):
+    input_vectors = np.random.uniform(-0.5 / dim, 0.5 / dim, (vocab_size, dim))
+    output_vectors = np.zeros((vocab_size, dim), dtype=float)
+
+    return input_vectors, output_vectors
+
+def forward(centre_word_id, context_word_id, negative_word_ids, input_vectors, output_vectors):
+    centre = input_vectors[centre_word_id]
+    context = output_vectors[context_word_id]
+    negatives = output_vectors[negative_word_ids]
+    positive_score = np.dot(context, centre)
+    negative_scores = np.matmul(negatives, centre)
+
+    return centre, context, negatives, positive_score, negative_scores
+
+
+def sigmoid(x):
+    result = np.zeros_like(x)
+    for i in range(len(x)):
+        if x[i] >= 0:
+            result[i] = 1 / (1 + np.exp(-x[i]))
+        else:
+            result[i] = np.exp(x[i]) / (1 + np.exp(x[i]))
+
+    return result
+
+def compute_loss(positive_score, negative_scores):
+    positive_loss = np.log(sigmoid(positive_score) + 1e-10)
+    negative_loss = np.sum(np.log(sigmoid(-negative_scores) + 1e-10))
+    total_loss = -(positive_loss + negative_loss)
+
+    return total_loss
+
+
+def compute_gradients(centre, context, negatives, positive_score, negative_scores):
+    context_error = sigmoid(positive_score) - 1
+    negative_error = sigmoid(negative_scores)
+    grad_context = context_error * centre
+    grad_negatives = negative_error.reshape(-1, 1) * centre
+    grad_centre = context_error * context + np.matmul(negative_error, negatives)
+
+    return grad_centre, grad_context, grad_negatives
+
+def update_parameters(centre_word_id, context_word_id, negative_word_ids,grad_centre, grad_context, grad_negatives,
+                      input_vectors, output_vectors, learning_rate):
+    input_vectors[centre_word_id] -= learning_rate * grad_centre
+    output_vectors[context_word_id] -= learning_rate * grad_context
+    output_vectors[negative_word_ids] -= learning_rate * grad_negatives
+
+    return input_vectors, output_vectors
